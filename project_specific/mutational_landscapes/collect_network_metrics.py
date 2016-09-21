709296aae2ad02b23aa59783c42b43822ecd0460
Rosangela Canino-Koning <rosiec@ronin>
2016-09-21 14:35:50 -0400

    fix

--- project_specific/mutational_landscapes/collect_network_metrics.py
+++ project_specific/mutational_landscapes/collect_network_metrics.py
@@ -176,6 +176,6 @@ for v in g.vertices():
 
         full_line = ",".join( [str(num_cpus[v]), line_fit, line_samep, 
                                line_diffp, line_whitenodes, line_blacknodes, 
-                               phenotypic_entropy])
+                               str(phenotypic_entropy)])
    
         print full_line
